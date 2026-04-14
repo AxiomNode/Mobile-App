c19dcb33b03942954026d59fea5f135c28e0e9f0
sebssgarcia502580 <sebssgarcia502580@gmail.com>
2026-04-14 22:45:36 +0200

    Fix Room KMP Apple database constructor

--- App/composeApp/src/commonMain/kotlin/es/sebas1705/axiomnode/data/db/AxiomNodeDatabase.kt
+++ App/composeApp/src/commonMain/kotlin/es/sebas1705/axiomnode/data/db/AxiomNodeDatabase.kt
@@ -1,7 +1,9 @@
 package es.sebas1705.axiomnode.data.db
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import es.sebas1705.axiomnode.data.entities.GameEntity
 import es.sebas1705.axiomnode.data.entities.GameResultEntity
 
@@ -10,8 +12,14 @@ import es.sebas1705.axiomnode.data.entities.GameResultEntity
     version = 1,
     exportSchema = false,
 )
+@ConstructedBy(AxiomNodeDatabaseConstructor::class)
 abstract class AxiomNodeDatabase : RoomDatabase() {
     abstract fun gameDao(): GameDao
     abstract fun gameResultDao(): GameResultDao
+}
+
+@Suppress("KotlinNoActualForExpect")
+expect object AxiomNodeDatabaseConstructor : RoomDatabaseConstructor<AxiomNodeDatabase> {
+    override fun initialize(): AxiomNodeDatabase
 }
 
